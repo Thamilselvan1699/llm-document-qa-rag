dbcc738867d025ceb2014e9ce879dd9e4ecf4a4c
Thamilselvan1699 <emailtothamil16@gmail.com>
2026-02-04 22:41:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-from src.rag_pipeline import answer_question
+import streamlit as st
+from src.load_docs import load_documents
+from src.embed_store import create_vector_store
+from src.rag_pipeline import generate_answer
 
-question = input("Ask a question: ")
-print(answer_question(question))
+st.title("LLM Document Q&A System")
+
+docs = load_documents("data/documents")
+vector_store = create_vector_store(docs)
+
+query = st.text_input("Ask a question based on documents")
+
+if query:
+    answer = generate_answer(query, vector_store)
+    st.write(answer)
